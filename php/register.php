9a786a6c58228471df4ae6482dfcc7f252f30311
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2019-10-09 14:17:09 -0300

    database connection + gathered form values (query coming soon)

--- php/register.php
+++ php/register.php
@@ -4,7 +4,21 @@ $username = "root";
 $password = "";
 $dbname = "helpdesk";
 
+//database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 return $conn;
 
+//TODO stil debating if should go procedural or object oriented.
+$naam = $_POST["naam"];
+$afdeling = $_POST["afdeling"];
+$datum_gebruik = $_POST["datum_gebruik"];
+$datum_terug = $_POST["datum_terug"];
+$gewenste_apparaat = $_POST["gewenste_apparaat"];
+$bijzonderheden = $_POST["getest_door"];
+$datum_test = $_POST["datum_test"];
+
+//sanitize values coming soon
+
+//have to finish query.
+$query = "INSERT INTO reserveringen (naam) values (.$naam.)";
 ?>
