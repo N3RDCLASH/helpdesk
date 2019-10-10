1d2e405750a70037e6c26fadb1df14dd50a262b8
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2019-10-10 12:44:25 -0300

    Updated register.php but not finished

--- php/register.php
+++ php/register.php
@@ -8,17 +8,50 @@ $dbname = "helpdesk";
 $conn = new mysqli($servername, $username, $password, $dbname);
 return $conn;
 
-//TODO stil debating if should go procedural or object oriented.
-$naam = $_POST["naam"];
-$afdeling = $_POST["afdeling"];
-$datum_gebruik = $_POST["datum_gebruik"];
-$datum_terug = $_POST["datum_terug"];
-$gewenste_apparaat = $_POST["gewenste_apparaat"];
-$bijzonderheden = $_POST["getest_door"];
-$datum_test = $_POST["datum_test"];
+if(isset($_POST["action"])){
+//sanitize query
+$naam = htmlspecialchars(strip_tags($_POST["naam"]));
+$afdeling = htmlspecialchars(strip_tags($_POST["afdeling"]));
+$datum_gebruik = htmlspecialchars(strip_tags($_POST["datum_gebruik"]));
+$datum_terug = htmlspecialchars(strip_tags($_POST["datum_terug"]));
+$gewenste_apparaat = htmlspecialchars(strip_tags($_POST["gewenste_apparaat"]));
+$bijzonderheden = htmlspecialchars(strip_tags($_POST["bijzonderheden"]));
+$getest_door = htmlspecialchars(strip_tags($_POST["getest_door"]));
+$datum_test = htmlspecialchars(strip_tags($_POST["datum_test"]));
 
 //sanitize values coming soon
 
+
 //have to finish query.
-$query = "INSERT INTO reserveringen (naam) values (.$naam.)";
+$query = "INSERT INTO 
+reserveringen 
+SET 
+naam=:naam,
+afdeling=:afdeling,
+datum_gebruik=:datum_gebruik,
+datum_terug=:datum_terug,
+gewenste_apparaat=:gewenste_apparaat,
+bijzonderheden=:bijzonderheden,
+getest_door=:getest_door,
+datum_test=:datum_test";
+
+$stmt = mysqli_connect($conn)->mysqli_prepare($query);
+
+if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+$stmt->mysqli_stmt_bind_param(":naam",$naam);
+$stmt->mysqli_stmt_bind_param(":afdeling",$afdeling);
+$stmt->mysqli_stmt_bind_param(":datum_gebruik",$datum_gebruik);
+$stmt->mysqli_stmt_bind_param(":datum_terug",$datum_terug);
+$stmt->mysqli_stmt_bind_param(":gewenste_apparaat",$gewenste_apparaat);
+$stmt->mysqli_stmt_bind_param(":bijzonderheden",$bijzonderheden);
+$stmt->mysqli_stmt_bind_param(":getest_door",$getest_door);
+$stmt->mysqli_stmt_bind_param(":datum_test",$datum_test);
+
+$stmt->mysqli_stmt_execute();
+  
+}
 ?>
