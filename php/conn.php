52db68b6effcac0ee29d1f406e28f7a979de816d
BR@5t0N <31829702+13A5T0N@users.noreply.github.com>
2019-10-12 00:52:52 -0300

    Add files via upload

---
+++ php/conn.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "helpdesk";
+$port = "3306";
+
+ //database connection
+ $link = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+?>
